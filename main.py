98ff1a58b1be3fdfdb5cfaf6373d76e9c2dc60f0
David Cao <dacao@protonmail.com>
2023-01-08 14:13:33 -0700

    list all cafes in the cafe.html in proper order

--- main.py
+++ main.py
@@ -1,11 +1,35 @@
+import os
+
+from dotenv import load_dotenv
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+
+from tables import Cafe, db
+
+load_dotenv()
 
 app = Flask(__name__)
 
 
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
+Bootstrap(app)
+
+# Connect to DB
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URI")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db.init_app(app)
+
+
 @app.route("/")
 def home():
-    return "Hello world"
+    return render_template("index.html")
+
+
+@app.route("/cafes")
+def cafes():
+    # Grab all cafes
+    all_cafes = Cafe.query.all()
+    return render_template("cafes.html", cafes=all_cafes)
 
 
 if __name__ == "__main__":
---
+++ tables.py
@@ -0,0 +1,28 @@
+import os
+
+from dotenv import load_dotenv
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+load_dotenv()
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URI")
+db = SQLAlchemy(app)
+
+
+class Cafe(db.Model):
+    __tablename__ = "cafe"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), unique=True, nullable=False)
+    map_url = db.Column(db.String(500), nullable=False)
+    img_url = db.Column(db.String(500), nullable=False)
+    location = db.Column(db.String(250), nullable=False)
+    seats = db.Column(db.String(250), nullable=False)
+    has_toilet = db.Column(db.Boolean, nullable=False)
+    has_wifi = db.Column(db.Boolean, nullable=False)
+    has_sockets = db.Column(db.Boolean, nullable=False)
+    can_take_calls = db.Column(db.Boolean, nullable=False)
+    coffee_price = db.Column(db.String(250), nullable=True)
